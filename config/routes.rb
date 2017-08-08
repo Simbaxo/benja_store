2708beef87d17607bc6823bff4c76eea0b3663da
Andres <ballerlikemike23@gmail.com>
2017-08-07 22:36:32 -0400

    Fixed layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
-	get '/portfolio', to: static_pages#portfolio'
-	get '/cart', to: static_pages#cart'
-	get '/about' to: static_pages#about'
+	get '/portfolio', to: 'static_pages#portfolio'
+	get '/cart', to: 'static_pages#cart'
+	get '/about', to: 'static_pages#about'
 end
